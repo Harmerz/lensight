e54b97f9634b2b1d70b5920339bb60413ec72b14
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-10-22 13:09:00 +0700

    feat : providers auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,8 @@
 import './globals.css'
 
-import { Inter } from 'next/font/google'
 import PropTypes from 'prop-types'
 
-const inter = Inter({ subsets: ['latin'] })
+import { Providers } from '@/components'
 
 export const metadata = {
   title: 'Create Next App',
@@ -13,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Providers>
+          <main>{children}</main>
+        </Providers>
+      </body>
     </html>
   )
 }
