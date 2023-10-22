5908e6dc4b13d0daad2bb0018a239f29b1e1113c
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-10-22 12:02:23 +0700

    feat : add hooks service

---
+++ src/hooks/question/mutation.js
@@ -0,0 +1,22 @@
+import { useAccessToken } from '@/hooks/auth'
+import { useApiMutation2 } from '@/hooks/useApiMutation'
+import { axios } from '@/lib/axios'
+
+export const useQuestions = () => {
+  const { accessToken, headers } = useAccessToken()
+
+  return useApiMutation2({
+    queryKey: ['questions'],
+    mutationFun: async (_, data) => {
+      if (!accessToken) {
+        return null
+      }
+      const res = await axios.post('/api/question', data, {
+        headers,
+      })
+      return res?.data?.data
+    },
+  })
+}
+
+export default useQuestions
