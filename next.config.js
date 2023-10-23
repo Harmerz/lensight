8128700584082ef6ab95b36c1aa3539d1b363d58
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-10-24 00:06:22 +0700

    feat : nextjs config delete loader Image

--- next.config.js
+++ next.config.js
@@ -1,28 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  images: {
-    loader: 'custom',
-    path: 'http://delightful-dune-098450410.4.azurestaticapps.net',
-    loaderFile: './src/components/loader.js',
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'delightful-dune-098450410.4.azurestaticapps.net',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lensights.my.id',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'drive.google.com',
-        port: '',
-      },
-    ],
-  },
+  // images: {
+  //   loader: 'custom',
+  //   path: 'http://delightful-dune-098450410.4.azurestaticapps.net',
+  //   loaderFile: './src/components/loader.js',
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'delightful-dune-098450410.4.azurestaticapps.net',
+  //       port: '',
+  //     },
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'lensights.my.id',
+  //       port: '',
+  //     },
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'drive.google.com',
+  //       port: '',
+  //     },
+  //   ],
+  // },
 }
 
 module.exports = nextConfig
