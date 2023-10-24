4634b1c5032f0b4a4ef06cfaecdc378f50d6d477
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-10-24 11:43:32 +0700

    feat : change question card

--- src/components/elements/answerCard.js
+++ src/components/elements/answerCard.js
@@ -1,8 +1,10 @@
+import { IoPersonCircleSharp } from 'react-icons/io5'
+
 export function AnswerCard({ answer }) {
   return (
     <div className="flex flex-row gap-5 rounded-2xl bg-eneutral-200 p-4">
-      <div className="flex h-6 w-6 rounded bg-neutral-700">a</div>
-      <p className="text-sm text-neutral-500">{answer}</p>
+      <IoPersonCircleSharp className="h-6 w-6 text-neutral-700" />
+      <p className="text-start text-sm text-neutral-500">{answer}</p>
     </div>
   )
 }
--- src/components/elements/questionCard.js
+++ src/components/elements/questionCard.js
@@ -1,7 +1,11 @@
+import Image from 'next/image'
+
 export function QuestionCard({ question, description, title }) {
   return (
-    <div className=" bg-neutral-1000 z-10 flex h-fit flex-row gap-5 rounded-2xl bg-opacity-50 p-5">
-      <div className="flex h-6 w-6 rounded bg-neutral-700">a</div>
+    <div className=" bg-neutral-1000 z-10 flex h-fit flex-row gap-5 rounded-2xl bg-opacity-50 p-5 text-start">
+      <div className="flex h-6 w-6 rounded">
+        <Image src="/assets/logo.png" alt="Logo" width={24} height={24} />
+      </div>
       <div className="flex w-fit grow flex-col gap-2">
         <h3 className="font-dmsans text-base text-neutral-700">{title}</h3>
         <h2 className=" font-dmsans text-2xl font-bold text-neutral-50">{question}</h2>
