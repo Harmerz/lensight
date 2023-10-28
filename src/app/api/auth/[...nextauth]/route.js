6c9a36ba6d48d1c23ad5882afa3eb78848bfbe94
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-10-28 15:48:02 +0700

    send next-auth url

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,5 +2,6 @@ import NextAuth from 'next-auth'
 
 import { options } from '@/auth'
 
+console.log(process.env.NEXTAUTH_URL)
 const handler = NextAuth(options)
 export { handler as GET, handler as POST }
